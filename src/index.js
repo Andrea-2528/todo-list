efe8df0e17a36f998067ac17760f597d0c201bfb
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-12-27 12:16:04 +0100

    Add basic template

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./styles.css";
-import { cry as message} from "./message";
-   
-const test = document.createElement("div");
-test.textContent = message;
-   
-document.body.appendChild(test);
